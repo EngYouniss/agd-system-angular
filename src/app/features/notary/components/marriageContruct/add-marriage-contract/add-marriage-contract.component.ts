354be85bf40be6026898619893f1f52636f71a1b
Younis Tallan <engyounistallan@gmail.com>
2025-08-16 02:46:34 +0300

    add the steper to add contract component and its design

--- src/app/features/notary/components/marriageContruct/add-marriage-contract/add-marriage-contract.component.ts
+++ src/app/features/notary/components/marriageContruct/add-marriage-contract/add-marriage-contract.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from "../../../layouts/navbar/navbar.component";
+import { StepperModule } from 'primeng/stepper';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-add-marriage-contract',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,StepperModule, ButtonModule],
   templateUrl: './add-marriage-contract.component.html',
   styleUrl: './add-marriage-contract.component.scss'
 })
